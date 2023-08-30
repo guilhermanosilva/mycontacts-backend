e595f7388ceff0e66a516d542f80384eae4064b5
Guilhermano Silva <guilhermanodev@gmail.com>
2023-08-30 11:46:45 -0300

    feat: adds categories api

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const ContactController = require('./controller/ContactController');
+const CategoryController = require('./controller/CategoryController');
 
 const router = Router();
 
@@ -9,5 +10,8 @@ router.post('/contacts', ContactController.store);
 router.get('/contacts/:id', ContactController.show);
 router.put('/contacts/:id', ContactController.update);
 router.delete('/contacts/:id', ContactController.delete);
+
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
 
 module.exports = router;
